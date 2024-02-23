ffe454e82513627d6bca5ae466090ba71bbb7277
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-02-22 21:55:02 -0500

    art.py added for ascii

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print('Welcome')
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encrypt(text, shift, direction):
@@ -18,7 +20,5 @@ def encrypt(text, shift, direction):
             else: new_i = i - shift
             ciphered += alphabet[new_i]
         print(ciphered)     
-# encrypt('civiliztion', 5, 'e')
-# encrypt('hnanqnefynts',5, 'd')
 
 encrypt(text = input("Type your message:\n").lower(),shift = int(input("Type the shift number:\n")), direction = input("Type 'e' to encrypt, type 'd' to decrypt:\n"))
