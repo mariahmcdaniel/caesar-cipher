805f0a22056a183efd65bfe197a0684e77714ef1
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-02-22 21:39:10 -0500

    simplifying code, no new lists needed

--- main.py
+++ main.py
@@ -4,37 +4,49 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-def encrypt(text,shift):
-    hold = []
-    new_hold = []
-    encrypted = []
-    for letter in str(text):
-        hold.append(alphabet.index(letter))
-    print(hold)    
-    for i in hold:
+# def encrypt(text,shift):
+#     hold = []
+#     new_hold = []
+#     encrypted = []
+#     for letter in str(text):
+#         hold.append(alphabet.index(letter))
+#     print(hold)    
+#     for i in hold:
+#         if i >= (26 - shift):
+#             new_hold.append(i-(26-shift))
+#         else: new_hold.append(i+shift)    
+#     print(new_hold)
+#     for num in new_hold:
+#         encrypted.append(alphabet[num])
+#     print(''.join(encrypted))    
+# encrypt('civilization',5)
+
+# def decrypt(text, shift):
+#     hold = []
+#     new_hold = []
+#     encrypted = []
+#     for letter in str(text):
+#         hold.append(alphabet.index(letter))
+#     print(hold)    
+#     for i in hold:
+#         if i <= (shift-1):
+#             new_hold.append(i+(26-shift))
+#         else: new_hold.append(i-shift)    
+#     print(new_hold)
+#     for num in new_hold:
+#         encrypted.append(alphabet[num])
+#     print(''.join(encrypted))
+
+# decrypt('hnanqnefynts',5)
+
+def encrypt(text, shift):
+    ciphered = ''
+    for letter in text:
+        i = alphabet.index(letter)
         if i >= (26 - shift):
-            new_hold.append(i-(26-shift))
-        else: new_hold.append(i+shift)    
-    print(new_hold)
-    for num in new_hold:
-        encrypted.append(alphabet[num])
-    print(''.join(encrypted))    
-encrypt('civilization',5)
+            new_i = i - (26-shift)
+        else: new_i = i + shift
+        ciphered += alphabet[new_i]
+    print(ciphered)        
 
-def decrypt(text, shift):
-    hold = []
-    new_hold = []
-    encrypted = []
-    for letter in str(text):
-        hold.append(alphabet.index(letter))
-    print(hold)    
-    for i in hold:
-        if i <= (shift-1):
-            new_hold.append(i+(26-shift))
-        else: new_hold.append(i-shift)    
-    print(new_hold)
-    for num in new_hold:
-        encrypted.append(alphabet[num])
-    print(''.join(encrypted))
-
-decrypt('hnanqnefynts',5)
+encrypt('civiliztion', 5)    
