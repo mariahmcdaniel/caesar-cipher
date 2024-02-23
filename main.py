48f4b189ce0734d9541397ebea4d6d42ac86299d
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2024-02-22 21:13:05 -0500

    working out logic

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-def encrypt(text):
+def encrypt(text,shift):
     hold = []
+    new_hold = []
     for letter in str(text):
         hold.append(alphabet.index(letter))
     print(hold)    
-
+    for i in hold:
+        if i >= (26 - shift):
 
 encrypt('civilization')
